6c0dc4b5cfd6415fe3bf8cc1be455976ba0b857f
Andrew Davis <atd285@gmail.com>
2023-01-18 15:30:50 -0500

    placeholders example

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,19 +1,24 @@
 use std::io;
 
 fn main() {
-    println!("Guess the number"!);
+    println!("Guess the number!");
 
     println!("Please input your guess.");
 
     // returns a new instance of string
     let mut guess = String::new();
     // the full purpose of this line is to read a line from the user and append it to the string guess
+    // the & indicates that this argument is a reference, which gives you a way to let multiple parts of your code access one piece of data without needing to copy that data into memory multiple times
     io::stdin().read_line(&mut guess).expect("Failed to read line");
 
+
+    // {} is a placeholder for a variable that will be printed later on. Think of {} as little crab princers that hold a value in place.
+    // When printing the value of a variable, the variable name can go inside the curly brackets. 
     println!("You guessed: {guess}");
 
     // creating variables
     // variables are immutable by default
-    let apples = 5;
-    let mut bananas = 5;
+    let x = 5;
+    let y = 10;
+    println!("x = {x} and y + 2 = {}", y + 2);
 }
