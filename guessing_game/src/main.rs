987b738b6ba70a766dda83544cdcb75a134c9869
Andrew Davis <atd285@gmail.com>
2023-01-19 14:04:26 -0500

    convert guess to a u32 bit integer

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,5 +1,6 @@
 use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
 fn main() {
     println!("Guess the number!");
     let secret_number = rand::thread_rng().gen_range(1..=100);
@@ -12,13 +13,19 @@ fn main() {
     // the & indicates that this argument is a reference, which gives you a way to let multiple parts of your code access one piece of data without needing to copy that data into memory multiple times
     io::stdin().read_line(&mut guess).expect("Failed to read line");
 
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
     // {} is a placeholder for a variable that will be printed later on. Think of {} as little crab princers that hold a value in place.
     // When printing the value of a variable, the variable name can go inside the curly brackets.
     println!("You guessed: {guess}");
 
     // creating variables
     // variables are immutable by default
-    let x = 5;
-    let y = 12;
-    println!("x = {x} and y + 2 = {}", y + 2);
+    // control flow
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
+
 }
