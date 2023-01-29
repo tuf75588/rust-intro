fd07ce8952f07fb7799b4172f354f32f55401ade
Andrew Davis <atd285@gmail.com>
2023-01-29 15:34:51 -0500

    macro_rules!

--- rust_concepts/src/main.rs
+++ rust_concepts/src/main.rs
@@ -1,6 +1,16 @@
+macro_rules! say_hello {
+    // `()` indicates that the macro takes no argument.
+    () => {
+        // The macro will expand into the contents of this block.
+        println!("Hello!");
+    };
+}
+
 fn main() {
     const NUM_CONSTANT: u32 = 6000 * 32;
     println!("Hello, world! {NUM_CONSTANT}");
+    say_hello!()
 }
+
 
 
