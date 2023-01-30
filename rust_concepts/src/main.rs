9e8d6871fbb9a67222d645e6e1931641335bcbb8
Andrew Davis <atd285@gmail.com>
2023-01-30 16:50:29 -0500

    64 bit and 32 bit floating point examples

--- rust_concepts/src/main.rs
+++ rust_concepts/src/main.rs
@@ -6,11 +6,23 @@ macro_rules! say_hello {
     };
 }
 
+// scalar types -- integers, floating-point numbers, booleans, and characters
+
+//  Integer Types
+//  Signed integers: i8, i16, i32, i64, i128 and isize (pointer size)   
+// unsigned integers: u8, u16, u32, u64, u128 and usize (pointer size) - unsigned integers can only store positive numbers
+
+
 fn main() {
     const NUM_CONSTANT: u32 = 6000 * 32;
     println!("Hello, world! {NUM_CONSTANT}");
     say_hello!()
+    let x = 2.0; // f64
+
+    let y: f32 = 3.0; // f32
+
 }
 
 
 
+ 
