21a0e2d389d055cd886715a3796e682584105fc4
Andrew Davis <atd285@gmail.com>
2023-01-31 16:55:49 -0500

    rust mathematical operations

--- rust_concepts/src/main.rs
+++ rust_concepts/src/main.rs
@@ -16,10 +16,54 @@ macro_rules! say_hello {
 fn main() {
     const NUM_CONSTANT: u32 = 6000 * 32;
     println!("Hello, world! {NUM_CONSTANT}");
-    say_hello!()
+    say_hello!();
     let x = 2.0; // f64
 
     let y: f32 = 3.0; // f32
+
+    // addition
+    let sum = 5 + 10;
+
+    // subtraction
+    let difference = 95.5 - 4.3;
+
+    // multiplication
+    let product = 4 * 30;
+
+    // division
+    let quotient = 56.7 / 32.2;
+
+    // remainder
+    let remainder = 43 % 5;
+
+    // boolean
+    let t = true;
+
+    let f: bool = false; // with explicit type annotation
+
+    // char
+    let c = 'z';
+    let z = 'ℤ';
+    let heart_eyed_cat = '😻';
+
+    // tuples
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+
+    let (x, y, z) = tup;
+
+    println!("The value of y is: {}", y);
+
+    let five_hundred = tup.0;
+
+    let six_point_four = tup.1;
+
+    let one = tup.2;
+
+    // arrays
+    let a = [1, 2, 3, 4, 5];
+
+    let first = a[0];
+    let second = a[1];
 
 }
 
