51520eee10e48efb6d4bfacab5239fdf461b1de1
Andrew Davis <atd285@gmail.com>
2023-02-13 16:44:34 -0500

    variable shadowing example

--- rust_concepts/src/main.rs
+++ rust_concepts/src/main.rs
@@ -73,6 +73,19 @@ fn main() {
     let a = [1, 2, 3, 4, 5];
     // statics 
     static LANGUAGE: &str = "Rust";
+
+
+    // variable shadowing examples
+    let x = 5;
+    let x = x + 1;
+
+    {
+        let x = x * 2;
+        println!("The inner scope value of x is: {x}");
+
+    }
+    println!("The value of x is: {x}")
+
 }
 
 
