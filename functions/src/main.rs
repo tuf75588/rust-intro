bb02843592d6c947fe3055e86362f716620a3aca
Andrew Davis <atd285@gmail.com>
2023-03-08 14:10:56 -0500

    function return values/types

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,8 +1,16 @@
 fn main() {
     println!("Hello, world!");
-    another_function(5)
+
+    let x = five();
+    let y = x_plus_one(65);
+    println!("The value of x is {x} {}", y);
 }
 
-fn another_function(x: i32) {
-    println!("The value of x is {x}");
+
+fn five() -> i32 {
+    5
+}
+
+fn x_plus_one(x:i32) -> i32 {
+    x + 1
 }
