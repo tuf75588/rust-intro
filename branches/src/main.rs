cad872b492dfc1f2e553cb239353f8e45ea73814
Andrew Davis <atd285@gmail.com>
2023-03-09 13:21:16 -0500

    using an if in a let statement

--- branches/src/main.rs
+++ branches/src/main.rs
@@ -1,8 +1,11 @@
 fn main() {
-   let num: i32 = 32;
+   let num: i32 = -5;
    if num > 10 {
     println!("{} is greater than 10", num)
    } else {
     println!("{} is less than 10", num)
    }
+
+   let number = if num > 10 { 10 } else { 5 };
+   println!("The value of number is: {number}");
 }
