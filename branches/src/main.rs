0b778e96db47fe5e211df659e9c6ba2653b3d14a
Andrew Davis <atd285@gmail.com>
2023-03-10 17:00:01 -0500

    example loop with break statement

--- branches/src/main.rs
+++ branches/src/main.rs
@@ -8,4 +8,14 @@ fn main() {
 
    let number = if num > 10 { 10 } else { 5 };
    println!("The value of number is: {number}");
+
+   let mut counter: i32 = 0;
+   let result = loop {
+      counter += 1;
+      if counter == 10 {
+         break counter * 2;
+         
+      }
+   };
+   println!("The result is {}", result);
 }
