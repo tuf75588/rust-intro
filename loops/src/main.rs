0fa1f432438e09cdc1d6dcbb12034257f38f9e58
Andrew Davis <atd285@gmail.com>
2023-03-13 15:49:13 -0400

    convert farenheit to celsius

--- loops/src/main.rs
+++ loops/src/main.rs
@@ -1,6 +1,9 @@
 fn main() {
-    for element in (0..4).rev() {
-        println!("element = {element}");
-    }
-    println!("LIFTOFF!")
+    let celsius = convert_to_c(59);
+    println!("celsius {}", celsius);
 }
+
+fn convert_to_c(f: i32) -> f64 {
+    ((f - 32) * 5/9).into()
+}
+
