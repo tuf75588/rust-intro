b296ea2924a5a6714b70e37f0fc1bcb26270ebf9
Andrew Davis <atd285@gmail.com>
2023-03-13 17:08:05 -0400

    fibonacci example

--- loops/src/main.rs
+++ loops/src/main.rs
@@ -1,9 +1,17 @@
 fn main() {
-    let celsius = convert_to_c(59);
-    println!("celsius {}", celsius);
+    // let celsius = convert_to_c(59);
+    // println!("celsius {}", celsius);
+    let fib_sequence = generate_fib_sequence(10);
 }
 
 fn convert_to_c(f: i32) -> f64 {
-    ((f - 32) * 5/9).into()
+    (((f - 32) * 5) / 9).into()
+}
+
+fn generate_fib_sequence(n: i32) {
+    if n == 0 {
+        1
+    }
+    
 }
 
