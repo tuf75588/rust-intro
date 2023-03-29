02c8106837d3f868a63369a006724eaa56d757a9
Andrew Davis <atd285@gmail.com>
2023-03-29 16:22:22 -0400

    hangman game logic

--- hangman_game/src/main.rs
+++ hangman_game/src/main.rs
@@ -4,15 +4,51 @@ use std::io;
 fn main() {
     // generate an array words that are all 5 letters long
     let words = [
-        "abbas", "abbes", "abbey", "abbot", "abeam", "abele", "abets", "abhor", "abide", "abies",
-        "abler", "ables", "abmho", "abode", "abohm", "aboil", "aboon", "abort", "about", "above",
-        "abrin", "abris", "abuna", "abuse",
+        "abbas",
+        "abbes",
+        "abbey",
+        "abbot",
+        "abeam",
+        "abele",
+        "abets",
+        "abhor",
+        "abide",
+        "abies",
+        "abler",
+        "ables",
+        "abmho",
+        "abode",
+        "abohm",
+        "aboil",
+        "aboon",
+        "abort",
+        "about",
+        "above",
+        "abrin",
+        "abris",
+        "abuna",
+        "abuse",
     ];
     let _random_num = rand::thread_rng().gen_range(0..words.len());
-    println!("Please guess a word");
+    println!("Please guess a word, {}", _random_num);
+    // pick a correct word
+    let correct_word = words[_random_num];
+    println!("The correct word is {}", correct_word);
     let mut guess = String::new();
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-    println!("you guessed {guess}")
+
+    // loop until the correct word is guessed
+    loop {
+        // get a guess from the user
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        // check if the guess is correct
+        if guess.trim() == correct_word {
+            println!("You guessed correctly!");
+            break;
+        } else {
+            println!("You guessed incorrectly!");
+        }
+    }
 }
