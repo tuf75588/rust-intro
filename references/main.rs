99c93f5ba406d0097e2ba03ee368f36376b79d4b
Andrew Davis <atd285@gmail.com>
2023-05-08 14:35:03 -0400

    mutable references

--- references/main.rs
+++ references/main.rs
@@ -1,6 +1,8 @@
 fn main() {
     let s1 = String::from("hello");
+    let mut s = String::from("hello");
     let len = calculate_length(&s1);
+    change(&mut s);
     println!("The length of '{}' is {}.", s1, len);
 }
 
@@ -12,3 +14,9 @@ fn calculate_length (s: &String) -> usize { // & is a reference
     // it is not dropped
 }
 
+// what happens when we try to mutate a reference?
+fn change(s: &mut String) {
+    s.push_str(", world");
+    // cannot borrow as mutable
+    // references are immutable by default
+}
