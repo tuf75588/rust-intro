34ce8c0d96f2dec418141582b4c0bfbeebba4bb5
Andrew Davis <atd285@gmail.com>
2023-05-31 16:54:26 -0400

    more string slice examples

--- the-slice-type/main.rs
+++ the-slice-type/main.rs
@@ -1,25 +1,24 @@
 fn main() {
-    let mut s = String::from("hello world");
-    let word = first_word(&s);
-
-    s.clear();
-
-
-    /* word is still available after we clear s */
-    println!("{word}");
+    let s: String = String::from("hello world");
+    /* a string slice */
+    /* is a part of a String */
+    let hello: &str = &s[0..2];
+    let world: &str = &s[6..11];
+    println!("{hello} {world}");
 
 
 }
 
 
-fn first_word(s: &String) -> usize {
-    let bytes: &[u8] = s.as_bytes();
-    for (_i, &item) in bytes.iter().enumerate() {
-      // check for a space character with byte literal syntax
-      if item == b' ' {
-        println!("{_i}");
-        return _i;
-      }
-    }
-    s.len()
-}
+// fn first_word(s: &String) -> usize {
+//     let bytes: &[u8] = s.as_bytes();
+//     for (_i, &item) in bytes.iter().enumerate() {
+//       // check for a space character with byte literal syntax
+//       if item == b' ' {
+//         println!("{_i}");
+//         return _i;
+//       }
+//     }
+//     s.len()
+// }
+
