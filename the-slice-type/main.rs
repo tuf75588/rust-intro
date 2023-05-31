2cb678614e1fe077e60b2e363274d29af9eaffe7
Andrew Davis <atd285@gmail.com>
2023-05-31 15:27:46 -0400

    finally understanding placeholders

--- the-slice-type/main.rs
+++ the-slice-type/main.rs
@@ -1,18 +1,6 @@
 fn main() {
-    println!("{}", first_word(&String::from("hello world")));
-    let str_slice: &[&str] = &["one", "two", "three"];
-    println!("{:?}", str_slice)
-}
-
-
-fn first_word(s: &String) -> usize {
-    let bytes = s.as_bytes();
-    println!("{}", bytes[0]);
-    for (i, &item) in bytes.iter().enumerate() {
-        if item == b' ' {
-            return i;
-        }
-    }
-
-    s.len()
+    let x = 5;
+    let y: i32 = 3;
+    let result = x + y;
+    println!("{}", result);
 }
