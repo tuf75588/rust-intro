7ec1d953938055d53028e86b59d8d2a71012ff10
Andrew Davis <atd285@gmail.com>
2023-05-31 16:21:57 -0400

    building off initial example

--- the-slice-type/main.rs
+++ the-slice-type/main.rs
@@ -1,6 +1,25 @@
 fn main() {
-    let x = 5;
-    let y: i32 = 3;
-    let result = x + y;
-    println!("{}", result);
+    let mut s = String::from("hello world");
+    let word = first_word(&s);
+
+    s.clear();
+
+
+    /* word is still available after we clear s */
+    println!("{word}");
+
+
+}
+
+
+fn first_word(s: &String) -> usize {
+    let bytes: &[u8] = s.as_bytes();
+    for (_i, &item) in bytes.iter().enumerate() {
+      // check for a space character with byte literal syntax
+      if item == b' ' {
+        println!("{_i}");
+        return _i;
+      }
+    }
+    s.len()
 }
