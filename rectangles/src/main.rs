71e408bbbd76270d396b057418de007ddd00ea94
Andrew Davis <atd285@gmail.com>
2023-06-22 16:17:02 -0400

    beyond basic use of structs

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -1,8 +1,20 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
 fn main() {
-    let rect1 = (30, 50);
-    println!("The area of the rectange is {} square pixels", area(rect1))
+    let scale = 2;
+    let rect1 = Rectangle {
+        width: dbg!(30 * scale),
+        height: 50,
+    };
+dbg!(&rect1);
+println!("{:?}", &rect1);
 }
 
-fn area(dimensions: (u32, u32)) -> u32 {
-    dimensions.0 * dimensions.1
+fn area(dimensions: &Rectangle) -> u32 {
+    dimensions.width * dimensions.height
 }
+
+// adding useful information with derived traits
