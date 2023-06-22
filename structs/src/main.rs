887979c2763c9dc16406bee18f28e99adeb2585a
Andrew Davis <atd285@gmail.com>
2023-06-22 12:48:10 -0400

    fix some errors

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -8,6 +8,8 @@ struct Post {
 struct User {
     email: String,
     username: String,
+    author: String,
+    sign_in_count: u64,
 }
 
 fn main() {
@@ -19,9 +21,29 @@ fn main() {
         email: String::from("atd285@gmail.com"),
         sign_in_count: 1,
     };
-    let mut user1 = build_user(String::from("atd285@gmail.com"), String::from("atd285"));
+    let mut user1 = build_user(
+        String::from("atd285@gmail.com"),
+        String::from("atd285"),
+        String::from("Andrew"),
+        55,
+    );
+    let mut post1: Post = build_post(true, String::from("andreŵ"), String::from("Andrew"), 55);
 }
 
-fn build_user(email: String, username: String) -> User {
-    User { email, username }
+fn build_user(email: String, username: String, author: String, sign_in_count: u64) -> User {
+    User {
+        email,
+        username,
+        author,
+        sign_in_count,
+    }
+}
+
+fn build_post(active: bool, author: String, email: String, sign_in_count: u64) -> Post {
+    Post {
+        active,
+        author,
+        email,
+        sign_in_count,
+    }
 }
