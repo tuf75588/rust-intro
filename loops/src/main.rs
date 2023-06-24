9560d00d64dea5c32d1cb27bddeecfd4dcce185b
Andrew Davis <atd285@gmail.com>
2023-06-24 13:21:11 -0400

    guarded return

--- loops/src/main.rs
+++ loops/src/main.rs
@@ -8,10 +8,9 @@ fn convert_to_c(f: i32) -> f64 {
     (((f - 32) * 5) / 9).into()
 }
 
-fn generate_fib_sequence(n: i32) {
-    if n == 0 {
-        1
+fn generate_fib_sequence(n: i32) -> i32 {
+    if n != 0 {
+        return;
     }
-    
+    0b1
 }
-
