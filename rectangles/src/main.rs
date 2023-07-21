ec0f382a69938c29634b6800ee9ea46d4bcdd70e
Andrew Davis <atd285@gmail.com>
2023-07-21 17:32:57 -0400

    associated functions

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -5,33 +5,17 @@ struct Rectangle {
 }
 
 impl Rectangle {
-    fn area(&self) -> u32 {
-        // method
-        self.width * self.height
-    }
-    fn width(&self) -> bool {
-        self.width > 0
-    }
-    fn can_hold(&self, other: &Rectangle) -> bool {
-       self.width > other.width && self.height > other.height 
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
     }
 }
 
 fn main() {
-    let rect1 = Rectangle {
-        width: 30,
-        height: 50,
-    };
-    let rect2 = Rectangle {
-        width: 10,
-        height: 40,
-    };
-    let rect3 = Rectangle {
-        width: 60,
-        height: 45,
-    };
-    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
-    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+    let sq = Rectangle::square(3);
+    println!("sq is {:?}", sq)
 }
 
 // adding useful information with derived traits
