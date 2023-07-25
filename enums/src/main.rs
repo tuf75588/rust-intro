aa40d31fe1d0c86487ca00c312f5655e6c01148d
Andrew Davis <atd285@gmail.com>
2023-07-25 15:02:31 -0400

    remove excess print line macros

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -14,10 +14,7 @@ impl Message {
 fn main() {
     let num = Some(5);
     let absent_number: Option<i32> = None;
-    println!("{absent_number:?}");
     let four = IPAddrKind::V4;
     let six = IPAddrKind::V6;
     let m = Message::Write(String::from("hello"));
-    m.call();
-    println!("Enums");
 }
