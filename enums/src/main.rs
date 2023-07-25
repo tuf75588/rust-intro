73c3e8b872398cf195e0e35634c41785620db62e
Andrew Davis <atd285@gmail.com>
2023-07-25 13:57:17 -0400

    impl example with enums

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -2,8 +2,19 @@ enum IPAddrKind {
     V4,
     V6,
 }
+
+enum Message {
+    Write(String),
+}
+impl Message {
+    fn call(&self) {
+        println!("Message");
+    }
+}
 fn main() {
     let four = IPAddrKind::V4;
     let six = IPAddrKind::V6;
+    let m = Message::Write(String::from("hello"));
+    m.call();
     println!("Enums");
 }
