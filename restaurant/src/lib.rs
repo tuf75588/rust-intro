eefbe138eca027080b7bbd08fd8785a878574a1f
Andrew Davis <atd285@gmail.com>
2023-11-16 12:57:44 -0500

    use keyword

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -1,12 +1,11 @@
 mod front_of_house {
     pub mod hosting {
-        pub fn add_to_waitlist() {
-            println!("Adding to waitlist!")
-        }
+        pub fn add_to_waitlist() {}
     }
 }
 
+use crate::front_of_house::hosting;
+
 pub fn eat_at_restaurant() {
-    // Absolute Path
-    crate::front_of_house::hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
 }
