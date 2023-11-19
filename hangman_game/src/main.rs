a7a532e62d9718db7c8c32c10c2e8642a7858dbe
Andrew Davis <atd285@gmail.com>
2023-11-19 15:07:43 -0500

    dont need this

--- hangman_game/src/main.rs
+++ hangman_game/src/main.rs
@@ -4,30 +4,9 @@ use std::io;
 fn main() {
     // generate an array words that are all 5 letters long
     let words = [
-        "abbas",
-        "abbes",
-        "abbey",
-        "abbot",
-        "abeam",
-        "abele",
-        "abets",
-        "abhor",
-        "abide",
-        "abies",
-        "abler",
-        "ables",
-        "abmho",
-        "abode",
-        "abohm",
-        "aboil",
-        "aboon",
-        "abort",
-        "about",
-        "above",
-        "abrin",
-        "abris",
-        "abuna",
-        "abuse",
+        "abbas", "abbes", "abbey", "abbot", "abeam", "abele", "abets", "abhor", "abide", "abies",
+        "abler", "ables", "abmho", "abode", "abohm", "aboil", "aboon", "abort", "about", "above",
+        "abrin", "abris", "abuna", "abuse",
     ];
     let _random_num = rand::thread_rng().gen_range(0..words.len());
     println!("Please guess a word, {}", _random_num);
