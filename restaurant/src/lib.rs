7c6cdda43078ea90615a111e1eb6082017cc26b4
Andrew Davis <atd285@gmail.com>
2023-11-20 16:26:57 -0500

    type aliases

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -1,10 +1,10 @@
-use std::fmt;
-use std::io;
+use std::fmt::Result;
+use std::fmt::Result as IOResult;
 
-fn function1() -> fmt::Result {
+fn function1() -> Result {
     println!("function1");
 }
 
-fn function2() -> io::Result<()> {
+fn function2() -> IOResult<()> {
     println!("function2");
 }
