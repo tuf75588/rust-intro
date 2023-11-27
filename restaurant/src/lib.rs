9aa7ac4ec1b501528f2e6d96630913afe737dcb4
Andrew Davis <atd285@gmail.com>
2023-11-27 15:38:17 -0500

    variable fonts

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -8,7 +8,9 @@ fn function1() -> Result {
 fn function2() -> IOResult<()> {
     println!("function2");
 }
-
+/* 
+one more thing
+*/
 mod front_of_house {
     pub mod hosting {
         pub fn add_to_waitlist() {
