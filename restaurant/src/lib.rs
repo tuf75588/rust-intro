5f36f3889929847597da868ecadc560bacfafdb3
Andrew Davis <atd285@gmail.com>
2023-11-27 15:13:34 -0500

    re-exporting module example

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -8,3 +8,17 @@ fn function1() -> Result {
 fn function2() -> IOResult<()> {
     println!("function2");
 }
+
+mod front_of_house {
+    pub mod hosting {
+        pub fn add_to_waitlist() {
+            println!("Adding to Waitlist");
+        }
+    }
+}
+
+pub use crate::front_of_house::hosting;
+
+pub fn eat_at_restaurant() {
+    hosting::add_to_waitlist();
+}
